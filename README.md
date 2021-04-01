1af9e310759d2656a9a728602f659872ad9d22a7
Ren Delaney <ren.delaney@gmail.com>
2021-04-01 23:34:39 +0100

    jackson & junit added, get & put requests built

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # URL Shortener
-## Java Assignment
 URL Shortener will allow users to pass a long URL and will be given a shortened URL.
 That shortened URL will redirect to the complete URL.
 
@@ -8,10 +7,12 @@ URL-Shortener will be built using the following technologies:
 - Java 16
 - Spring 2.4.4
 - Maven 3.6.3
+- JUnit 5.7.0
+- jackson 2.12.2
 - Docker
 
 Progress on this project can be seen on the [Trello] board
-User stories can be viewed on the [Miro] board
+User stories & UML diagrams can be viewed on the [Miro] board
 
 [Trello]: <https://trello.com/b/XV99y2JP>
 [Miro]: https://miro.com/app/board/o9J_lMOnXfU=/
--- src/main/java/com/example/demo/LinkController.java
+++ src/main/java/com/example/demo/LinkController.java
@@ -1,20 +1,113 @@
 package com.example.demo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 public class LinkController {
 
     private final AtomicLong counter = new AtomicLong();
 
     @PostMapping("/shorten-link")
-    Link addLink(@RequestBody Link link) {
-        Link newLink = new Link(counter.incrementAndGet(), "test", fullLink, "test");
-        newLink.setShortLink(newLink.generateShortLink());
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Link addLink(@RequestBody Link link) throws JsonMappingException, JsonProcessingException {
+        String shortLink = link.generateShortLink();
+        //if link not unique, loop until unique short-link generated
+        //while(checkLinkIsUnique(shortLink) == false){
+           shortLink = link.generateShortLink();
+        //}
+        Link newLink = new Link(counter.incrementAndGet(), link.getName(), link.getLink(), shortLink);
+        try {
+            saveLinkToFile(newLink);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 		return newLink;
 	}
+
+    @GetMapping("/links")
+    public String getLinks() throws FileNotFoundException, JsonMappingException, JsonProcessingException {
+        File file = new File("target/link.json");
+        Scanner scanner = new Scanner(file).useDelimiter("\\Z");
+        String jsonFileContent = scanner.next();
+        scanner.close();
+        //final ObjectMapper objectMapper = new ObjectMapper();
+        //Link[] links = objectMapper.readValue(jsonFileContent, Link[].class);
+        return jsonFileContent;
+    }
+
+    public Link saveLinkToFile(Link link) throws JsonGenerationException, JsonMappingException, IOException{
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file = new File("target/link.json");
+        String jsonFormatting = null;
+        
+        if(file.length() == 0){ // empty file, add starting square bracket
+            jsonFormatting = "[";
+        }else{ //JSON already in file, add comma to separate JSON objects
+            jsonFormatting = ",";
+            //remove current closing bracket by removing final character from file
+            Scanner scanner = new Scanner(file).useDelimiter("\\Z");
+            String jsonFileContent = scanner.next();
+            scanner.close();
+            String withoutLastCharacter = jsonFileContent.substring(0, jsonFileContent.length() - 1);
+            FileWriter myWriter = new FileWriter("target/link.json");
+            myWriter.write(withoutLastCharacter);
+            myWriter.close();
+        }
+        OutputStream os = new FileOutputStream(file, true);
+        //convert object to JSON string with json formatter
+        String jsonInString = jsonFormatting + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(link);
+        //write json to file
+        os.write(jsonInString.getBytes(), 0, jsonInString.length());
+        //close square bracket
+        os.write("]".getBytes(), 0, "]".length());
+        os.close();
+        return link;
+    }
+
+    public boolean checkLinkIsUnique(String shortLink) throws JsonMappingException, JsonProcessingException{
+        Boolean result = true;
+        //pull Link objects from links.json
+        File file = new File("target/link.json");
+        String jsonFromFile;
+        try {
+            Scanner scanner = new Scanner(file).useDelimiter("\\Z");
+            jsonFromFile = scanner.next();
+            scanner.close();
+            final ObjectMapper objectMapper = new ObjectMapper();
+            Link[] links = objectMapper.readValue(jsonFromFile, Link[].class);
+            
+            //loop through existing links to check this short-link doesn't match
+            for (Link link : links) {
+                if(link.getShortLink() == shortLink){
+                    result = false;
+                    break;
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
